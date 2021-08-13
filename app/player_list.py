ec252136576217079be544ec354c66740acb70a1
Bradley Torpy <20058303@tafe.wa.edu.au>
2021-08-13 10:36:54 +0800

    Added player_list.py.

---
+++ app/player_list.py
@@ -0,0 +1,15 @@
+# Filename      :   player_list.py
+# Location      :  ./app
+# Project       :   SRUS-BT-Games
+# Author        :   Bradley Torpy <200583083@tafe.wa.edu.au>
+# Created       :   13/08/2021
+# Version       :   0.1
+# Description   :
+#   This is a description for the file, and I am lazy.
+#
+
+
+class PlayerList:
+    def __init__(self):
+        self.head = None
+
